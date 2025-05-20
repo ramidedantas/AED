6db02c1468782ddc65fafa34d9a87bd4748a9e15
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2025-05-20 14:58:43 -0300

    Aula-01: código exemplo pilha de execução

--- Aula-01/main.cpp
+++ Aula-01/main.cpp
@@ -4,21 +4,42 @@ using namespace std;
 
 typedef unsigned long long ull;
 
-int main() {
-    ull a, b, n;
-    ull soma = 0;
+struct Obj {
+    int x = 2;
+    int y = 3;
+};
 
-    std::cin >> a; // scanf("%d", &a);
-    std::cin >> b;
+void func2(Obj ooo) {
+    int v = ooo.y;
+    ooo.x = v * 2;
+}
+
+void func1(int p, Obj oo) {
+    int k = p;
+    if (p == 0) {
+        return;
+    }
+    func1(p - 1, oo);
+}
+
+int main() {
+    int i = 5;  Obj o1;
+    func1(i, o1);
+
+//    ull a, b, n;
+//    ull soma = 0;
+//
+//    std::cin >> a; // scanf("%d", &a);
+//    std::cin >> b;
 
 //    for (unsigned long long i = a; i <= b; i++) {
 //        soma += i;
 //    }
 
-    n = b - a + 1;
-    soma = n * (a + b) / 2;
-
-    cout << soma << endl;
+//    n = b - a + 1;
+//    soma = n * (a + b) / 2;
+//
+//    cout << soma << endl;
 
     return 0;
 }
